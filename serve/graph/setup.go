8f2c8fb6531fcf370e954f8d7597339c47f04e33
jinoosss <jwchoi@onbloc.xyz>
2025-02-25 21:25:35 +0900

    fix: graphql setup

--- serve/graph/setup.go
+++ serve/graph/setup.go
@@ -13,7 +13,6 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/go-chi/chi/v5"
 	"github.com/vektah/gqlparser/v2/ast"
-	"github.com/vektah/gqlparser/v2/gqlerror"
 
 	"github.com/gnolang/tx-indexer/events"
 	"github.com/gnolang/tx-indexer/serve/graph/model"
@@ -41,25 +40,6 @@ func Setup(s storage.Storage, manager *events.Manager, m *chi.Mux) *chi.Mux {
 	))
 
 	srv.AddTransport(&transport.Websocket{})
-
-	srv.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
-		oc := graphql.GetOperationContext(ctx)
-		if oc.Operation != nil && oc.Operation.Operation == ast.Query {
-			if includesIntrospection(oc.Operation.SelectionSet) {
-				return func(ctx context.Context) *graphql.Response {
-					return &graphql.Response{
-						Errors: gqlerror.List{
-							&gqlerror.Error{
-								Message: "GraphQL introspection is disabled",
-							},
-						},
-					}
-				}
-			}
-		}
-
-		return next(ctx)
-	})
 
 	es, err := examplesToSlice()
 	if err != nil {
@@ -111,29 +91,4 @@ func newGraphQueryServer(es graphql.ExecutableSchema) *handler.Server {
 	})
 
 	return srv
-}
-
-func includesIntrospection(selectionSet ast.SelectionSet) bool {
-	for _, selection := range selectionSet {
-		switch sel := selection.(type) {
-		case *ast.Field:
-			if sel.Name == "__schema" || sel.Name == "__typename" {
-				return true
-			}
-
-			if includesIntrospection(sel.SelectionSet) {
-				return true
-			}
-		case *ast.FragmentSpread:
-			if sel.Definition != nil && includesIntrospection(sel.Definition.SelectionSet) {
-				return true
-			}
-		case *ast.InlineFragment:
-			if includesIntrospection(sel.SelectionSet) {
-				return true
-			}
-		}
-	}
-
-	return false
 }
